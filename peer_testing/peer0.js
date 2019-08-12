184460661749d64bd2627aa099eaef2aac72ef66
Vincent Tran <VincentTran197120@gmail.com>
2019-08-11 23:05:34 -0500

    Destructured the imports

--- peer_testing/peer0.js
+++ peer_testing/peer0.js
@@ -1,6 +1,6 @@
-const node_module = require('../p2p/peer');
+const { node } = require('../p2p/peer');
 
-let node1 = new node_module.node('localhost',4000);
+let node1 = new node('localhost',4000);
 node1.connectToPeer('localhost',3000);
 node1.sendTestMessage();
 node1.sendCustomMessage({hi: "Hello"});
--- peer_testing/peer1.js
+++ peer_testing/peer1.js
@@ -1,4 +1,4 @@
-const node_module = require('../p2p/peer');
+const { node } = require('../p2p/peer');
 
-let node2 = new node_module.node('localhost',3000);
+let node2 = new node('localhost',3000);
 node2.connectToPeer('localhost',4000);
