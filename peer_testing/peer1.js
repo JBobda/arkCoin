5185deed834c3fb549a767bdb25f204b4b72809f
Vincent Tran <VincentTran197120@gmail.com>
2019-08-11 19:40:12 -0500

    Fully functional peer to peer communication

--- peer_testing/peer1.js
+++ peer_testing/peer1.js
@@ -1,14 +1,23 @@
 const p2p = require('p2p');
 
 const peer = p2p.peer({
-    host: 'localhost',
+    host: 'localhost', //Local IP or 'localhost'
     port: 3000,
     wellKnownPeers: { host: 'localhost', port: 4000 } //Peers
 });
 
-peer.remote({
-    host: 'localhost',
-    port: 4000
-  }).run('handle/foo', { foo: 'bar' }, (err, result) => {
-    // ...
+//console.log(peer.wellKnownPeers.get()[0]);
+//0 = Self
+//1 = localhost:4000
+
+let peerSelected = peer.wellKnownPeers.get()[1];
+peer.remote(
+  peerSelected
+).run('handle/blockData', { data: 'value' }, (err, result) => {
+    if(err != null){
+      console.log(err);
+    }
+    else{
+      console.log("<<---Data is sent to " + peerSelected.host+ ":" + peerSelected.port+"--->>");
+    }
 });
--- peer_testing/peer2.js
+++ peer_testing/peer2.js
@@ -1,13 +1,14 @@
+//Receiving Peer
+
 const p2p = require('p2p');
 
 const peer = p2p.peer({
-    host: 'localhost',
+    host: 'localhost', 
     port: 4000,
     wellKnownPeers: { host: 'localhost', port: 3000 } //Peers
 });
 
-peer.handle.foo = (payload, done) => {
-    console.log(payload);
+peer.handle.blockData = (data, done) => {
+    console.log(data);
     done(null);
-    // or: done(null, result);
 };
